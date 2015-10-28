20b9fc0c92835ba455e43429cc4514b612f5ebb8
python490 <python490@gmail.com>
2015-10-28 11:39:02 -0700

    Descriptions for Prob 6 and 7.

--- Prob7/prob7.py
+++ Prob7/prob7.py
@@ -1,4 +1,11 @@
-is_prime = lambda x: all(x%i !=0 for i in range(int(x**0.5)+1)[2:])
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+
 
 def solver():
     count = 0
@@ -11,3 +18,7 @@ def solver():
     return answer
 
 print solver()
+
+
+
+is_prime = lambda x: all(x%i !=0 for i in range(int(x**0.5)+1)[2:])
